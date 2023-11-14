b03305e210e395ff08d4d47afde0a20bd2616a91
vinicius <andrade.vinicius934@gmail.com>
2023-11-13 22:42:34 -0300

    generator start

--- src/generator/main.c
+++ src/generator/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <assert.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char** argv) {
+    assert(argc == 3);
+
+    const char* input = argv[INPUT_ARG_INDEX];
+
+    FILE* file = fopen(input, "w+");
+    if (!file) {
+        perror(strerror(errno));
+        return 1;
+    }
+
+    const char* text = "sexo";
+    const unsigned long written = fwrite(text, 1, strlen(text), file);
+    printf("Wrote %lu - %s to %s\n", written, text, input);
+
     return 0;
 }
