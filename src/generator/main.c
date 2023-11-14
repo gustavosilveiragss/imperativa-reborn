5b11667891e7f68ada3ea9732ac19f81d1f679e7
vinicius <andrade.vinicius934@gmail.com>
2023-11-14 20:28:30 -0300

    abstraction

--- src/generator/main.c
+++ src/generator/main.c
@@ -8,14 +8,13 @@ int main(int argc, char** argv) {
     assert(argc == 3);
 
     const char* input = argv[INPUT_ARG_INDEX];
-
     FILE* file = fopen(input, "w+");
     if (!file) {
         perror(strerror(errno));
         return 1;
     }
 
-    const char* text = "sexo";
+    const char* text = "12";
     const unsigned long written = fwrite(text, 1, strlen(text), file);
     printf("Wrote %lu - %s to %s\n", written, text, input);
 
