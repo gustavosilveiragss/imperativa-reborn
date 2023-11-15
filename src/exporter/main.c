64b43d5dff11f545277f28c3c66ea380346dd4d8
Gustavo Silveira <gustavo24gss@gmail.com>
2023-11-15 11:11:01 -0300

    exporter with mock data

--- src/exporter/main.c
+++ src/exporter/main.c
@@ -1,36 +1,53 @@
+#include <assert.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <shared/args.h>
 #include <shared/Account.h>
 #include <shared/AccountNode.h>
 
-int main() {
-    // create three accounts
+int main(int argc, char** argv) {
+    assert(argc == 3);
+    
+    // Read binary
+    // const char* input = argv[INPUT_ARG_INDEX];
+    // FILE* file = fopen(input, "w+");
+    // if (!file) {
+    //     perror(strerror(errno));
+    //     return 1;
+    // }
+
+    // Write data into a linked list
+
+    // -------- MOCK DATA --------
     Account acc1 = { 1, "John", 1000, 1000, "a@gmail.com", "1234567890", "1234567890" };
     Account acc2 = { 2, "Mary", 1000, 1000, "a@gmail.com", "1234567890", "1234567890" };
     Account acc3 = { 3, "Peter", 1000, 1000, "a@gmail.com", "1234567890", "1234567890" };
-
-    // create a doubly linked list
     AccountNode* head = createAccountNode(acc1);
-    AccountNode* acc2Node = prepend(&head, acc2);
+    AccountNode* acc2Node = append(&head, acc2);
     append(&acc2Node, acc3);
 
-    // display the list
-    displayList(head);
+    // Write data into an output txt file
 
-    printf("\n");
+    const char* output = argv[OUTPUT_ARG_INDEX];
+    FILE* outputFile = fopen(output, "w");
+    if (!outputFile) {
+        perror(strerror(errno));
+        return 1;
+    }
 
-    // sort list
-    sort(&head);
+    for (AccountNode* curr = head; curr != NULL; curr = curr->next) {
+        fprintf(outputFile, "%zu %s %zu %f %s %s %s\n",
+               curr->data.id,
+               curr->data.name,
+               curr->data.level,
+               curr->data.balance,
+               curr->data.email,
+               curr->data.creationDate,
+               curr->data.lastLoginDate);
+    }
 
-    // display the list
-    displayList(head);
-
-    // delete second node
-    deleteNode(&head, acc2Node);
-
-    printf("\n");
-
-    // display the list
-    displayList(head);
+    fclose(outputFile);
 
     return 0;
 }
